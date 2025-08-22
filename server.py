fe92df5369162edc959fceaf0aefdf6e53388640
loganlang393 <langewischl@gmail.com>
2025-08-22 02:57:12 -0400

    Used pylint to perform a static code anlysis on the server.py file

--- server.py
+++ server.py
@@ -1,3 +1,12 @@
+"""
+server.py
+
+This module contains the configuration of the Flask web application 
+for the emotion detection project. It handles incoming requests, and 
+either processes text input to return an emotional analysis of the 
+input, or displays the template for the main page.
+"""
+
 from flask import Flask, request, render_template
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,10 +14,27 @@ app = Flask("EmotionDetector")
 
 @app.route("/")
 def index_route():
+    """
+    Displays the web application that is constructed in the index.html in the root route /
+
+    Returns:
+        template: index.html
+    """
+
     return render_template('index.html')
 
 @app.route("/emotionDetector")
 def detect_emotion():
+    """
+    Detects the emotions present within the text provided.
+
+    Args:
+        text_to_analyze (str): the text provided by the user to be analyzed
+
+    Returns:
+        str: a string constructed by the response from the Emotion Detection API
+    """
+
     text_to_analyze = request.args.get('textToAnalyze')
 
     response = emotion_detector(text_to_analyze)
@@ -16,7 +42,14 @@ def detect_emotion():
     if response['dominant_emotion'] is None:
         return "Invalid text! Please try again"
 
-    reply = f"For the given statement, the system response is 'anger': {response['anger']}, 'disgust': {response['disgust']}, 'fear': {response['fear']}, 'joy': {response['joy']} and 'sadness': {response['sadness']}. The dominant emotion is {response['dominant_emotion']}."
+    reply = ("For the given statement, the system response is "
+    f"'anger': {response['anger']}, "
+    f"'disgust': {response['disgust']}, "
+    f"'fear': {response['fear']}, "
+    f"'joy': {response['joy']} "
+    f"and 'sadness': {response['sadness']}. "
+    f"The dominant emotion is {response['dominant_emotion']}."
+    )
 
     return reply
 
